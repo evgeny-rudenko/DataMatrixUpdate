d0fe53faef32bb08ae5e3d3c073f5570e0d1ca31
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2021-06-16 19:31:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace DataMatrixUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbGood = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDatamatrix = new System.Windows.Forms.TextBox();
+            this.gridKiz = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rtbQuery = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridKiz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbGood
+            // 
+            this.tbGood.Location = new System.Drawing.Point(159, 12);
+            this.tbGood.Name = "tbGood";
+            this.tbGood.Size = new System.Drawing.Size(616, 20);
+            this.tbGood.TabIndex = 0;
+            this.tbGood.TextChanged += new System.EventHandler(this.tbGood_TextChanged);
+            this.tbGood.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbGood_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Строка для поиска товаров";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Заменить КИЗ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Отсканируйте новый код";
+            // 
+            // tbDatamatrix
+            // 
+            this.tbDatamatrix.Location = new System.Drawing.Point(159, 55);
+            this.tbDatamatrix.Name = "tbDatamatrix";
+            this.tbDatamatrix.Size = new System.Drawing.Size(616, 20);
+            this.tbDatamatrix.TabIndex = 4;
+            // 
+            // gridKiz
+            // 
+            this.gridKiz.AllowUserToAddRows = false;
+            this.gridKiz.AllowUserToDeleteRows = false;
+            this.gridKiz.AllowUserToOrderColumns = true;
+            this.gridKiz.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridKiz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridKiz.Location = new System.Drawing.Point(15, 156);
+            this.gridKiz.MultiSelect = false;
+            this.gridKiz.Name = "gridKiz";
+            this.gridKiz.ReadOnly = true;
+            this.gridKiz.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridKiz.Size = new System.Drawing.Size(760, 150);
+            this.gridKiz.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(610, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 44);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Заполнить таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtbQuery
+            // 
+            this.rtbQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbQuery.Location = new System.Drawing.Point(15, 312);
+            this.rtbQuery.Name = "rtbQuery";
+            this.rtbQuery.Size = new System.Drawing.Size(760, 126);
+            this.rtbQuery.TabIndex = 7;
+            this.rtbQuery.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(134, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(641, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "СКАНИРОВАТЬ ТОЛЬКО В АНГЛИЙСКОЙ РАСКЛАДКЕ !!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbQuery);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.gridKiz);
+            this.Controls.Add(this.tbDatamatrix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbGood);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Замена КИЗ в базе данных F3Tail";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.gridKiz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGood;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDatamatrix;
+        private System.Windows.Forms.DataGridView gridKiz;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox rtbQuery;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace DataMatrixUpdate
+{
+    public partial class Form1 : Form
+    {
+        public static DataTable fillDataTable(string query)
+        {
+
+            String conSTR = Properties.Settings.Default.ConnectionString;
+            SqlConnection sqlConn = new SqlConnection(conSTR);
+            sqlConn.Open();
+            SqlCommand cmd = new SqlCommand(query, sqlConn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //var dataReader = cmd.ExecuteReader(;
+            //dt.Load()
+
+            //dt.Load(dataReader);
+
+            //sqlConn.Close();
+            //da.Dispose();
+            return dt;
+            /*
+             SqlCommand cmd = new SqlCommand(query, sqlConn);
+            cmd.CommandTimeout = 0;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+           
+            var dataReader = cmd.ExecuteReader();
+            dt.Load(dataReader);
+            ///
+            sqlConn.Close();
+            Console.WriteLine(DateTime.Now.ToString());
+            return dt;
+            */
+        }
+
+        public  string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        
+
+        public  string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadKiz();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbDatamatrix.Text.Length <30)
+            {
+                MessageBox.Show("Некорректная длина кода датаматрикс");
+                return;
+            }
+            
+
+            string DatamatrixBase64 = "";
+            string GTIN_SGTIN = "";
+            string GTIN = "";
+            string SGTIN = "";
+            string ID_KIZ_GLOBAL;
+            GTIN = tbDatamatrix.Text.Substring(2, 14);
+            SGTIN = tbDatamatrix.Text.Substring(18, 13);
+            GTIN_SGTIN = GTIN + SGTIN;
+
+            //this.gridKiz.Rows[this.gridKiz.CurrentRow.Index].Cells["ID_KIZ_GLOBAL"].Value
+            //if (this.gridKiz.Rows[this.gridKiz.SelectedRows.Count].Cells["ID_KIZ_GLOBAL"].Value != null)
+            if (this.gridKiz.Rows[this.gridKiz.CurrentRow.Index].Cells["ID_KIZ_GLOBAL"].Value != null)
+            {
+                //MessageBox.Show(this.gridKiz.Rows[this.gridKiz.CurrentRow.Index].Cells["ID_KIZ_GLOBAL"].Value.ToString());
+                ID_KIZ_GLOBAL = this.gridKiz.Rows[this.gridKiz.CurrentRow.Index].Cells["ID_KIZ_GLOBAL"].Value.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Что то пошло не так");
+                return;
+            }
+
+            DatamatrixBase64 = Base64Encode(tbDatamatrix.Text);
+            
+            rtbQuery.AppendText(Environment.NewLine);
+            rtbQuery.AppendText(DatamatrixBase64);
+            rtbQuery.AppendText(Environment.NewLine);
+            rtbQuery.AppendText(Base64Decode(DatamatrixBase64));
+            rtbQuery.AppendText(Environment.NewLine);
+            rtbQuery.AppendText(GTIN);
+            rtbQuery.AppendText(Environment.NewLine);
+            rtbQuery.AppendText(SGTIN);
+            rtbQuery.AppendText(Environment.NewLine);
+            rtbQuery.AppendText(GTIN_SGTIN);
+
+
+            //return; // пока не записываем ничего в бд
+
+
+            var connetionString = Properties.Settings.Default.ConnectionString;
+            var sql = File.ReadAllText("KIZUpdate.SQL"); //"UPDATE Employees SET LastName = @LastName, FirstName = @FirstName, Title = @Title ... ";// repeat for all variables
+            try
+            {
+                using (var connection = new SqlConnection(connetionString))
+                {
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        /*
+                         BARCODE=@DatamatrixBase64, GTIN_SGTIN = @GTIN_SGTIN , SGTIN = @SGTIN, GTIN = @GTIN
+                         */
+                        command.Parameters.Add("@DatamatrixBase64", SqlDbType.NVarChar).Value = DatamatrixBase64;
+                        command.Parameters.Add("@GTIN_SGTIN", SqlDbType.NVarChar).Value = GTIN_SGTIN;
+                        command.Parameters.Add("@GTIN", SqlDbType.NVarChar).Value = GTIN;
+                        command.Parameters.Add("@SGTIN", SqlDbType.NVarChar).Value = SGTIN;
+                        command.Parameters.Add("@ID_KIZ_GLOBAL", SqlDbType.NVarChar).Value = ID_KIZ_GLOBAL;
+                        //command.Parameters.Add("@Title", SqlDbType.NVarChar).Value = Titelstring;
+                        // repeat for all variables....
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show($"Failed to update. Error message: {ee.Message}");
+            }
+            finally
+            {
+                MessageBox.Show("КИЗ обновлен. Попробуйте отсканировать в АРМ кассира");
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadKiz();
+        }
+
+
+
+        public void LoadKiz()
+        {
+            
+            
+            string SQLQuery = File.ReadAllText("KIZGoods.SQL");
+            SQLQuery =  SQLQuery.Replace("GOODNAMESEARCH", "%"+tbGood.Text+"%");
+
+            rtbQuery.AppendText(SQLQuery);
+            DataTable kiz = fillDataTable(SQLQuery);
+            this.gridKiz.DataSource = kiz;
+            this.gridKiz.Refresh();
+
+        }
+
+        private void tbGood_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             if (e.KeyChar == (char) Keys.Enter)
+            {
+                LoadKiz();
+            }
+
+        }
+
+        private void tbGood_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
